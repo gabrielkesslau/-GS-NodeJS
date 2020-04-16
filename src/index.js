bcd69c9c9d4bf94f0f266835f24762950c5f0ab3
gabrielkesslau <33272479+gabrielkesslau@users.noreply.github.com>
2020-04-16 15:10:56 -0300

    ajuste estrutura

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const app = express();
-const cors = require('cors')
+const cors = require('cors');
 const { uuid, isUuid } = require('uuidv4');
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());

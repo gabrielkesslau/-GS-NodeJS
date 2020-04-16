4b0b474bbbc2a8892c0d60359902c9b8129b37aa
gabrielkesslau <33272479+gabrielkesslau@users.noreply.github.com>
2020-04-16 14:37:27 -0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const { uuid, isUuid } = require('uuidv4');
 
+app.use(cors());
 app.use(express.json());
 
 // const livereload = require('livereload');
